bda277e941ce9f140c6b039f6a1a1a6007d94831
ingframin <framin2@gmail.com>
2023-11-17 10:24:54 +0100

    put loadTexture code in display.c

--- display.c
+++ display.c
@@ -1,5 +1,15 @@
 #include "display.h"
 
+SDL_Texture* loadTexture(SDL_Renderer* renderer, const char* filename){
+    SDL_Texture* txt;
+    SDL_Surface* tmp = IMG_Load(filename);
+    Uint32 colorkey = SDL_MapRGB( tmp->format, 100, 100, 100 );
+    SDL_SetColorKey(tmp,SDL_TRUE,colorkey);
+    txt = SDL_CreateTextureFromSurface(renderer,tmp);
+    SDL_FreeSurface(tmp);
+    return txt;
+}
+
 Display* newDisplay(const char* title, int width, int height){
     Display* disp = (Display*)malloc(sizeof(Display));
     disp->wnd = SDL_CreateWindow(title,SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,width,height,SDL_WINDOW_SHOWN);
